f7ce39520001a6b133f12e7f9e38bb7946f084bd
Ian Greulich <ian.greulich@gmail.com>
2013-03-06 15:57:31 -0500

    add typeahead module

---
+++ build/igTypeahead.js
@@ -0,0 +1,23 @@
+angular.module('igTypeahead', []).directive('typeahead', function(){
+  return{
+    restrict: 'E',
+    replace: true,
+    scope: {
+      choice: '=',
+      list: '='
+    },
+    template: '<input type="text" ng-model="choice" />',
+    link: function(scope, element, attrs){
+      scope.typeaheadElement = element;
+      $(element).typeahead({
+        source: scope.list,
+        updater: function(item){
+          scope.$apply(function(){
+            scope.choice = item;
+          });
+          return item;
+        }
+      });
+    }
+  };
+});
